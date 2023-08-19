720c611dd40b8c6b8e8c9c67f2fffd2b3f79eba8
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-08-19 21:11:57 +0530

    sidebar added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
-  const items = ['Contacts', 'Charts & Maps']
   return (
-    <div className="bg-gray-800 text-white h-full w-1/5 p-6 fixed top-0 left-0">
-      <h2 className="text-xl font-semibold mb-4">CovidSync</h2>
+    <div className="bg-gray-800 text-white h-full w-1/5 pt-6 fixed top-0 left-0">
+      <h2 className="text-xl font-semibold my-3 text-center">CovidSync</h2>
       <ul className="space-y-2">
         <Link to={'/'}>
-          <li>Contacts</li>
+          <li className="hover:bg-slate-600 px-2 py-2 sm:px-10 sm:py-5">
+            Contacts
+          </li>
         </Link>
         <Link to={'/covid-stats'}>
-          <li>Charts & Maps</li>
+          <li className="hover:bg-slate-600 px-2 py-2 sm:px-10 sm:py-5">
+            Charts & Maps
+          </li>
         </Link>
       </ul>
     </div>
